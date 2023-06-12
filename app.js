6b7c277dd681564835c896270ed349ec19228e83
Galang Ramadhan <galang.rambu.ramadhan.99@gmail.com>
2023-06-13 04:33:17 +0700

    update foodList (error)

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const Sequelize = require('sequelize');
 const cors = require('cors');
 const moment = require('moment');
 const path = require('path');
+const { QueryTypes } = require('sequelize');
 const jwt = require('jsonwebtoken');
 
 dotenv.config();
@@ -388,7 +389,6 @@ app.post('/postFood', authenticateToken, upload.single('fotoMakanan'), async (re
   }
 });
 
-
 // Get all history
 app.get('/history', authenticateToken, (req, res) => {
   const userId = req.user.userId;
@@ -447,14 +447,19 @@ app.put('/history/:id', authenticateToken, (req, res) => {
     });
 });
 
-// API route for viewing available food list '/foodList'
-app.get('/foodList', authenticateToken, (req, res) => {
+// API route for viewing food list by userId '/foodList/userId'
+app.get('/foodList/:userId', authenticateToken, (req, res) => {
+  const userId = req.params.userId;
+
   Food.findAll({
-    attributes: ['foodId', 'fotoMakanan', 'foodName', 'description', 'quantity', 'location', 'latitude', 'longitude', 'expiredAt', 'foodType']
+    attributes: ['foodId', 'name', 'expiredAt', 'fotoMakanan', 'userId'],
+    where: {
+      userId: userId,
+    },
   })
     .then((food) => {
       if (food.length === 0) {
-        res.status(200).json({ message: 'Tidak ada makanan yang didonasikan' });
+        res.status(200).json({ message: 'Tidak ada makanan yang didonasikan oleh pengguna ini' });
       } else {
         res.json(food);
       }
@@ -464,6 +469,89 @@ app.get('/foodList', authenticateToken, (req, res) => {
       res.status(500).json({ error: 'Internal server error' });
     });
 });
+
+// API route for viewing available food list '/foodList' //ERROR
+app.get('/foodList', authenticateToken, async (req, res) => {
+    try {
+      // Get User ID from authenticationToken
+      const userId = get_user_id_from_auth_token(authenticateToken);
+      
+      // Get ML parameter
+      const foodTypes = await sequelize.query(
+        `SELECT f.foodType FROM history h
+         JOIN food f ON f.foodId = h.foodId
+         JOIN user u ON u.userId = h.userId_peminat
+         WHERE u.userId = :userID`,
+        {
+          replacements: { userId: userId },
+          type: QueryTypes.SELECT,
+        }
+      );
+  
+      const parameterML = new Map();
+      parameterML.set("ayam_dagingT", 0);
+      parameterML.set("ikan_seafoodT", 0);
+      parameterML.set("tahu_tempe_telurT", 0);
+      parameterML.set("sayurT", 0);
+      parameterML.set("sambalT", 0);
+      parameterML.set("nasi_mie_pastaT", 0);
+      parameterML.set("sop_soto_baksoT", 0);
+      parameterML.set("kue_rotiT", 0);
+      parameterML.set("jajanan_pasarT", 0);
+      parameterML.set("puding_jeliT", 0);
+      parameterML.set("keripik_kerupukT", 0);
+      parameterML.set("buah_minumanT", 0);
+  
+      for (const f of foodTypes) {
+        if (f.foodType === 'ayam_dagingT') {
+          parameterML.set("ayam_dagingT", parameterML.get("ayam_dagingT") + 1);
+        } else if (f.foodType === 'ikan_seafoodT') {
+          parameterML.set("ikan_seafoodT", parameterML.get("ikan_seafoodT") + 1);
+        } else if (f.foodType === 'tahu_tempe_telurT') {
+          parameterML.set("tahu_tempe_telurT", parameterML.get("tahu_tempe_telurT") + 1);
+        } else if (f.foodType === 'sayurT') {
+          parameterML.set("sayurT", parameterML.get("sayurT") + 1);
+        } else if (f.foodType === 'sambalT') {
+          parameterML.set("sambalT", parameterML.get("sambalT") + 1);
+        } else if (f.foodType === 'nasi_mie_pastaT') {
+          parameterML.set("nasi_mie_pastaT", parameterML.get("nasi_mie_pastaT") + 1);
+        } else if (f.foodType === 'sop_soto_baksoT') {
+          parameterML.set("sop_soto_baksoT", parameterML.get("sop_soto_baksoT") + 1);
+        } else if (f.foodType === 'kue_rotiT') {
+          parameterML.set("kue_rotiT", parameterML.get("kue_rotiT") + 1);
+        } else if (f.foodType === 'jajanan_pasarT') {
+          parameterML.set("jajanan_pasarT", parameterML.get("jajanan_pasarT") + 1);
+        } else if (f.foodType === 'puding_jeliT') {
+          parameterML.set("puding_jeliT", parameterML.get("puding_jeliT") + 1);
+        } else if (f.foodType === 'keripik_kerupukT') {
+          parameterML.set("keripik_kerupukT", parameterML.get("keripik_kerupukT") + 1);
+        } else if (f.foodType === 'buah_minumanT') {
+          parameterML.set("buah_minumanT", parameterML.get("buah_minumanT") + 1);
+        }
+      }
+  
+      // API call ke API ML
+      const preferensi_user = await http.post('https://wasteless-api-v1-ywnxbyxnda-et.a.run.app/', parameterML); // Example
+  
+      // Ambil seluruh makanan dari database
+      const foods = await Food.findAll({
+        where: {
+          foodType: preferensi_user,
+        },
+        limit: 4,
+      });
+  
+      // Memunculkan semua makanan
+      if (foods.length === 0) {
+        res.status(200).json({ message: 'Tidak ada makanan yang didonasikan' });
+      } else {
+        res.json(foods);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });  
 
 // API route for viewing details of a specific available food '/foodDetail/:id'
 app.get('/foodDetail/:id', authenticateToken, (req, res) => {
@@ -586,6 +674,23 @@ function authenticateToken(req, res, next) {
     req.user = user;
     next();
   });
+}
+
+function get_user_id_from_auth_token(token) {
+  try {
+    // Ensure the token is provided as a string
+    if (typeof token !== 'string') {
+      throw new Error('Invalid token');
+    }
+
+    // Verify and decode the token to get the user ID
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    return decodedToken.userId;
+  } catch (error) {
+    // Handle token verification error
+    console.error('Token verification error:', error);
+    return null;
+  }
 }
 
 // Generate JWT token
